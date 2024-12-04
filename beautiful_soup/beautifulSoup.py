bd8f01fe0e9f3a16b5408ff4131d5ed020531d19
Carey99 <edwinakidah1@gmail.com>
2024-12-04 12:22:08 +0300

    Beautiful Soup usage example added

---
+++ beautiful_soup/beautifulSoup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    We use Beautiful soup to operate easily with html contents
+    for instance Getting specific text from <div> or <p> tags
+"""
+import requests
+from bs4 import BeautifulSoup
+
+#let's make a GET request
+
+r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+
+#Optionally we can log to see if the request was successful
+print(r)
+
+#Now let's try Parsing some HTML
+soup = BeautifulSoup(r.content, 'html.parser')
+print(soup.prettify()) #The prettiffy function when called, returns a Unicoded string
+
+#Instead of printing, this time we shall wrrite it inside a file
+
+with open('/home/queen/Web_scrapping/pettified.html', 'w', encoding='UTF-8') as f:
+    f.write(soup.prettify())
